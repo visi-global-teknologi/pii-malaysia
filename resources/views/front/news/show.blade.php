017b10d3fbf936ba2f03d295c67af7b194fe5af3
latief <la790x@gmail.com>
2022-11-28 15:43:21 +0700

    form comment

--- resources/views/front/news/show.blade.php
+++ resources/views/front/news/show.blade.php
@@ -37,20 +37,20 @@
                             <form action="">
                                 <div class="row">
                                     <div class="col-md-6 form-group">
-                                        <input name="name" type="text" class="form-control" placeholder="Your Name*">
+                                        <input required name="name" type="text" class="form-control" placeholder="Your Name*">
                                     </div>
                                     <div class="col-md-6 form-group">
-                                        <input name="email" type="text" class="form-control" placeholder="Your Email*">
+                                        <input required name="email" type="email" class="form-control" placeholder="Your Email*">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col form-group">
-                                        <input name="website" type="text" class="form-control" placeholder="Your Website">
+                                        <input name="website" type="url" class="form-control" placeholder="Your Website">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col form-group">
-                                        <textarea name="comment" class="form-control" placeholder="Your Comment*"></textarea>
+                                        <textarea required name="comment" class="form-control" placeholder="Your Comment*"></textarea>
                                     </div>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Post Comment</button>
