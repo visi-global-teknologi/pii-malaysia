3949b034024275485834514f26442a4c2de325e1
latief <la790x@gmail.com>
2023-03-17 10:17:48 +0700

    strip tags phone number

--- resources/views/layouts/upconstruction/footer.blade.php
+++ resources/views/layouts/upconstruction/footer.blade.php
@@ -6,7 +6,7 @@
                 <div class="col-lg-4 col-md-6">
                     <div class="footer-info">
                         <h3>{{ strtoupper(config('app.name')) }}</h3>
-                        <p>{{ array_key_exists('contact_address', $data['homepage_lists_contact']) ? strip_tags($data['homepage_lists_contact']['contact_address']) : "" }}<br><br><strong>Phone:</strong> {{ array_key_exists('contact_phone_number', $data['homepage_lists_contact']) ? $data['homepage_lists_contact']['contact_phone_number'] : "" }}<br> <strong>Email:</strong> {{ array_key_exists('contact_email', $data['homepage_lists_contact']) ? strip_tags($data['homepage_lists_contact']['contact_email']) : "" }}<br> </p>
+                        <p>{{ array_key_exists('contact_address', $data['homepage_lists_contact']) ? strip_tags($data['homepage_lists_contact']['contact_address']) : "" }}<br><br><strong>Phone:</strong> {{ array_key_exists('contact_phone_number', $data['homepage_lists_contact']) ? strip_tags($data['homepage_lists_contact']['contact_phone_number']) : "" }}<br> <strong>Email:</strong> {{ array_key_exists('contact_email', $data['homepage_lists_contact']) ? strip_tags($data['homepage_lists_contact']['contact_email']) : "" }}<br> </p>
                         <div class="social-links d-flex mt-3"> <a href="{{ array_key_exists('contact_twitter', $data['homepage_lists_contact']) ? $data['homepage_lists_contact']['contact_twitter'] : "" }} " class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a> <a href="{{ array_key_exists('contact_facebook', $data['homepage_lists_contact']) ? $data['homepage_lists_contact']['contact_facebook'] : "" }}" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a> <a href="{{ array_key_exists('contact_instagram', $data['homepage_lists_contact']) ? $data['homepage_lists_contact']['contact_instagram'] : "" }}" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a></div>
                     </div>
                 </div>
@@ -20,7 +20,6 @@
                 <!-- All the links in the footer should remain intact. -->
                 <!-- You can delete the links only if you purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/ --> Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
             </div>
         </div>
     </div>
