17d0e0f7106b104b4ef7aa62ef10dafa06dee2f2
latief <la790x@gmail.com>
2023-03-17 10:23:25 +0700

    strip tags phone number

--- resources/views/layouts/upconstruction/footer.blade.php
+++ resources/views/layouts/upconstruction/footer.blade.php
@@ -17,9 +17,6 @@
         <div class="container">
             <div class="copyright"> &copy; Copyright <strong><span>{{ ucwords(config('app.name')) }}</span></strong>. All Rights Reserved </div>
             <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
             </div>
         </div>
     </div>
